6dbb8a7edc198925e53b3c3021ae7c3b8e433199
DarKSoul Atik <anantoshahrear10@gmail.com>
2025-06-07 03:15:53 +0600

    first commit

---
+++ src/Routes/router.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div className="">hi</div>,
+  },
+]);
+
+export default router;
---
+++ src/tailwind.config.mjs
@@ -0,0 +1,18 @@
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        bgDark: '#1e2835',
+        textSoft: '#f99e72',
+        heading: '#dd4b4b',
+        secondary: '#f5eddf',
+      },
+    },
+  },
+  plugins: [],
+};
